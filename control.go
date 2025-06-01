b369cf46b1bdc3c4ba1e50b97b89deddc5265a1d
Adam J. Richardson <fatman.uk@gmail.com>
2025-06-01 13:55:37 +0100

    Implement CallWheel

--- control.go
+++ control.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+	"strconv"
+	"fatgo/callwheel"
 )
 
 type Control struct {
@@ -9,21 +12,47 @@ type Control struct {
 	view *View
 }
 
+func refFn(cw *callwheel.CallWheel, cr *ConfigRepo, view *View) {
+	view.log(LL_DEBUG, "refreshing")
+	cr.pull()
+	cw.Insert(cr.Refresh, func() {
+		refFn(cw, cr, view)
+	})
+}
+
 func (re Control) main() {
+	epoch := time.Now()
 	configRepo, err := configFactory(re.args)
 	if err != nil {
 		re.view.log(LL_ERROR, err.Error())
-		re.alive = false
+		return
 	}
-	vault, err := vaultFactory(configRepo)
-	if err != nil {
-		re.view.log(LL_ERROR, err.Error())
-		re.alive = false
-	}
-	if vault.head() != 200 {
-		re.view.log(LL_ERROR, "Vault isn't responding.")
-		re.alive = false
-	}
+
+	cw := callwheel.CallWheel{Size: 10}
+	cw.Begin()
+	defer cw.End()
+
+	re.view.log(LL_DEBUG, "Refresh: " + strconv.Itoa(configRepo.Refresh))
+
+	cw.Insert(configRepo.Refresh, func() {
+		refFn(&cw, &configRepo, re.view)
+	})
+
+//	vault, err := vaultFactory(configRepo)
+//	if err != nil {
+//		re.view.log(LL_ERROR, err.Error())
+//		return
+//	}
+//	if vault.head() != 200 {
+//		re.view.log(LL_ERROR, "Vault isn't responding.")
+//		return
+//	}
 	for re.alive {
+		thisEpoch := time.Now()
+		if thisEpoch.Sub(epoch).Milliseconds() >= 1000 {
+			epoch = thisEpoch
+			re.view.log(LL_INFO, "tik")
+			cw.Tick()
+		}
 	}
 }
